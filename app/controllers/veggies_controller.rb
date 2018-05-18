cb05165aa3a134d66516ac625de2d0f1fcbfa523
Maartje <m.j.oconnor88@gmail.com>
2018-05-18 11:24:18 +0200

    Added users to veggies and added validations

--- app/controllers/veggies_controller.rb
+++ app/controllers/veggies_controller.rb
@@ -1,49 +1,50 @@
 class VeggiesController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
 
-def index
-  @veggies = Veggy.all
-end
+  def index
+    @veggies = Veggy.all
+  end
 
-def show
-  @veggy = Veggy.find(params[:id])
-end
-
-def new
-  @veggy = Veggy.new
-end
-
-def create
-  @veggy =  Veggy.new(veggy_params)
-    if @veggy.save
-      redirect_to veggies_path
-    else
-      render :new
-    end
-end
-
-def edit
+  def show
     @veggy = Veggy.find(params[:id])
   end
 
-def update
-  @veggy = Veggy.find(params[:id])
+  def new
+    @veggy = Veggy.new
+  end
+
+  def create
+    @veggy = current_user.veggies.new(veggy_params)
+    if @veggy.save
+      redirect_to veggies_path, notice: 'Veggy was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def edit
+      @veggy = Veggy.find(params[:id])
+    end
+
+  def update
+    @veggy = Veggy.find(params[:id])
     if @veggy.update(veggy_params)
       redirect_to veggies_path
     else
       render :edit
     end
-end
+  end
 
-def destroy
-  @veggy = Veggy.find(params[:id])
-  @veggy.destroy
-  redirect_to veggies_url
-end  
+  def destroy
+    @veggy = Veggy.find(params[:id])
+    @veggy.destroy
+    redirect_to veggies_url
+  end
 
-private
+  private
 
-def veggy_params
-  params.require(:veggy).permit(:name)
-end
+  def veggy_params
+    params.require(:veggy).permit(:name)
+  end
 
 end
--- app/models/veggy.rb
+++ app/models/veggy.rb
@@ -1,2 +1,5 @@
 class Veggy < ApplicationRecord
+  belongs_to :user
+
+  validates :user, presence: true
 end
